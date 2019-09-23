6de868280eb9e17ce17c9daa6de33d3aedc577ca
ViperAshutosh <53410164+ViperAshutosh@users.noreply.github.com>
2019-09-23 12:14:34 +0530

    Update hack.py

--- stdplugins/hack.py
+++ stdplugins/hack.py
@@ -1,41 +1,18 @@
-"""Emoji
-
-Available Commands:
-
-.emoji shrug
-
-.emoji apple
-
-.emoji :/
-
-.emoji -_-"""
+"""Hack Animation
+Command : .hack"""
 
 from telethon import events
-
 import asyncio
 
-
-
-
-
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 async def _(event):
-
     if event.fwd_from:
-
         return
-
     animation_interval = 2.5
-
     animation_ttl = range(0, 11)
-
     input_str = event.pattern_match.group(1)
-
     if input_str == "hack":
-
         await event.edit(input_str)
-
         animation_chars = [
         
             "`Connecting To Private Telegram Server...`",
@@ -48,11 +25,10 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████████████████████ `",
-            "`Targeted Account Successfully Hacked...\n\nPay 69$ To @arnab431 To Remove This Hack and take your acc back`"
+            "`Targeted Account Successfully Hacked...\n\nPay @techsndtipsowner ₹69K To Remove This Hack and take your acc back`"
         ]
 
         for i in animation_ttl:
-
             await asyncio.sleep(animation_interval)
-
             await event.edit(animation_chars[i % 11])
+            
